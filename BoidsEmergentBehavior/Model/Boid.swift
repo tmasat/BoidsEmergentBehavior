aae67c8e3a6dc3d160c22b4c340607468009c91b
Çağrı Tuğberk Masat <tugberkmasat@yahoo.com>
2025-06-21 16:23:59 +0300

    Set up project foundation for boids simulation

---
+++ BoidsEmergentBehavior/Model/Boid.swift
@@ -0,0 +1,14 @@
+//
+//  Boid.swift
+//  BoidsEmergentBehavior
+//
+//  Created by Tugberk Masat on 21.06.2025.
+//
+
+import Foundation
+import simd
+
+struct Boid {
+    var position: float2
+    var velocity: float2
+} 
