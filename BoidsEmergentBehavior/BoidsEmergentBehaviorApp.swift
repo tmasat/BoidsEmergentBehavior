4808a45ad2e80018098243514caa43c962430b9d
Çağrı Tuğberk Masat <tugberkmasat@yahoo.com>
2025-06-21 16:10:25 +0300

    init project

---
+++ BoidsEmergentBehavior/BoidsEmergentBehaviorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoidsEmergentBehaviorApp.swift
+//  BoidsEmergentBehavior
+//
+//  Created by Tugberk Masat on 21.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct BoidsEmergentBehaviorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
